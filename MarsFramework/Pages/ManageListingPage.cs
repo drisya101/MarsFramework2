c5e8918320f699d58b79686e118ab4a9fceabbdd
Drisya Tharuvana <72536229+Drisya-mtn@users.noreply.github.com>
2021-09-09 20:23:29 +1200

    Finished task 2

---
+++ MarsFramework/Pages/ManageListingPage.cs
@@ -0,0 +1,149 @@
+﻿using MarsFramework.Global;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+
+namespace MarsFramework.Pages
+{
+    internal class ManageListingsPage : PageBase
+    {
+        public ManageListingsPage(IWebDriver driver) : base(driver)
+        {
+            PageFactory.InitElements(Global.GlobalDefinitions.driver, this);
+        }
+
+        //Click on Manage Listings Link
+        [FindsBy(How = How.LinkText, Using = "Manage Listings")]
+        private IWebElement ManageListings { get; set; }
+
+        //View the listing
+        [FindsBy(How = How.XPath, Using = "(//i[@class='eye icon'])[1]")]
+        private IWebElement View { get; set; }
+
+        //Delete the listing
+        [FindsBy(How = How.XPath, Using = "//*[@id='listing-management-section']/div[2]/div[1]/div[1]/table/tbody/tr[1]/td[8]/div/button[3]")]
+        private IWebElement Delete { get; set; }
+
+        //Edit the listing
+        [FindsBy(How = How.XPath, Using = "(//i[@class='outline write icon'])[1]")]
+        private IWebElement Edit { get; set; }
+
+        //Edit Title
+        [FindsBy(How = How.Name, Using = "title")]
+        private IWebElement EditTitle { get; set; }
+
+        //Edit Description
+        [FindsBy(How = How.Name, Using = "description")]
+        private IWebElement EditDescription { get; set; }
+
+
+
+
+        //Click on Yes or No
+        [FindsBy(How = How.XPath, Using = "//div[@class='actions']")]
+        private IWebElement clickActionsButton { get; set; }
+
+        //Alter Box
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div")]
+        private IWebElement AlertBox { get; set; }
+
+
+
+
+        [FindsBy(How = How.Name, Using = "isActive")]
+        private IWebElement ActiveCheckbox { get; set; }
+
+
+        //Click Yes
+        //[FindsBy(How = How.CssSelector, Using = "button.ui.icon.positive.right.labeled.button")]
+        //private IWebElement ClickYes { get; set; }
+
+        //Click Yes
+        [FindsBy(How = How.XPath, Using = "//body[@class='dimmable dimmed']/div[2]//div[@class='actions']/button[2]")]
+        private IWebElement ClickYes { get; set; }
+
+
+        //Save button
+        [FindsBy(How = How.XPath, Using = "//*[@id='service-listing-section']/div[2]/div/form/div[11]/div/input[1]")]
+        private IWebElement Savebutton { get; set; }
+
+        [FindsBy(How = How.CssSelector, Using = "div.active.section")]
+        private IWebElement ViewListingTitleLabel { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='listing-management-section']/div[2]/div[1]/div[1]/table/tbody/tr/td[3]")]
+        private IWebElement EditedTitle { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "//*[@id='listing-management-section']/div[2]/h3")]
+        private IWebElement NoServiceListed { get; set; }
+
+
+
+        internal void NavigateToViewListedServices()
+        {
+            Click(ManageListings);
+            Click(View);
+
+        }
+
+
+
+        internal void EditExistingService(string newTitle)
+        {
+            Click(ManageListings);
+            Click(Edit);
+            EnterText(EditTitle, newTitle);
+            EnterText(EditDescription, "This is edited Description");
+            Click(Savebutton);
+
+        }
+
+        internal void TestDeleteServices()
+        {
+            Click(ManageListings);
+            Click(Delete);
+            WaitAndFindElement(AlertBox);
+
+            //Pop up box
+            Click(ClickYes);
+
+        }
+
+
+
+
+
+        internal void Listings()
+        {
+            //Populate the Excel Sheet
+            GlobalDefinitions.ExcelLib.PopulateInCollection(Base.ExcelPath, "ManageListings");
+
+
+        }
+
+        //Check  Listing Active
+
+        public bool IsListingActive()
+        {
+            return ActiveCheckbox.Enabled;
+        }
+
+        //View the Page Title
+        public string GetViewPageTitle()
+        {
+            return GetText(ViewListingTitleLabel);
+        }
+
+        //Edit get the listing title
+        public string GetListingTitle()
+        {
+            return GetText(EditedTitle);
+        }
+
+        //Delete get null
+        public string NoServiceListing()
+        {
+            return GetText(NoServiceListed);
+        }
+    }
+}
---
+++ MarsFramework/Test/MarsTests.cs
@@ -0,0 +1,69 @@
+﻿using Excel.Log;
+using MarsFramework.Global;
+using MarsFramework.Pages;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using RelevantCodes.ExtentReports;
+using System;
+
+namespace MarsFramework
+{
+    [TestFixture]
+    [Category("Sprint1")]
+    class MarsTests : Base
+    {
+        ShareSkillPage _shareSkillPage;
+        ManageListingsPage _manageListingsPage;
+        IWebDriver _driver;
+
+        [SetUp]
+        public void Setup()
+        {
+            _driver = GlobalDefinitions.driver;
+            ExtentReports();
+            test = extent.StartTest(TestContext.CurrentContext.Test.FullName);
+            _shareSkillPage = new ShareSkillPage(_driver);
+            _manageListingsPage = new ManageListingsPage(_driver);
+        }
+        //Test Services listing
+
+        [Test, Description("Test entering skill share data in share skill page"), Order(1)]
+        public void TestEnterServiesListing()
+        {
+            _shareSkillPage.EnterSkillDetails();
+            Assert.True(_manageListingsPage.IsListingActive(), "Adding a listing failed");
+        }
+
+        //Test view
+
+        [Test, Description("Test user can view entered skill share data in manage listing page"), Order(2)]
+        public void TestViewListedServices()
+        {
+            _manageListingsPage.NavigateToViewListedServices();
+            Assert.AreEqual("This is a title", _manageListingsPage.GetViewPageTitle());
+        }
+
+        //Test Edit
+        [Test, Description("Test user can edit entered skill share data in manage listing page"), Order(3)]
+        public void TestEditServices()
+        {
+            var expectedTitle = "This is a edited title";
+            _manageListingsPage.EditExistingService(expectedTitle);
+            Assert.AreEqual(expectedTitle, _manageListingsPage.GetListingTitle());
+
+        }
+
+
+        //Test Delete
+        [Test, Description("Test user can delete entered skill share data in manage listing page"), Order(4)]
+        public void TestDeleteServices()
+        {
+            var expectedText = "You do not have any service listings!";
+            _manageListingsPage.TestDeleteServices();
+            Assert.AreEqual(expectedText, _manageListingsPage.NoServiceListing());
+
+        }
+
+
+    }
+}
